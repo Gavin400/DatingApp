701cd5158e53a3365de3c728ed5f9677778ae1b4
Gavin400 <perisgavin@gmail.com>
2022-08-03 00:25:47 +0530

    End of section 9

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -14,32 +14,23 @@ import { AccountService } from '../_services/account.service';
 export class NavComponent implements OnInit {
 
   model:any={}
-  // loggedIn:boolean;
-  // currentUser$:Observable<User>;
+  
 
   constructor(public accountService:AccountService, private router:Router, private toastr:ToastrService) { }
 
   ngOnInit(): void {
-    // this.getCurrentUser();
-    // this.currentUser$=this.accountService.currentUser$;
+   
   }
   login() {
     this.accountService.login(this.model).subscribe(response => {
       this.router.navigateByUrl('/members');
-      // this.loggedIn = true;
+      
     })
   }
   logout() {
     this.accountService.logout();
     this.router.navigateByUrl('/');
-    // this.loggedIn = false;
+    
   }
-  // getCurrentUser(){
-  //   this.accountService.currentUser$.subscribe(user => {
-  //     this.loggedIn = !!user;
-  //   },error =>{
-  //     console.log(error);
-  //   })
-  // }
-
+  
 }
